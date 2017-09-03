d4a2d207586ea33473380a7f54290c82a86e64f4
zxh326 <zhangxh1997@gmail.com>
2017-09-03 23:48:40 +0800

    add Concurrent

--- Something/server.py
+++ Something/server.py
@@ -1,16 +1,14 @@
-import socket
-sk = socket.socket()
-sk.bind(("127.0.0.1",8888))
-sk.listen(5)
-#rec
-while True:	
-	conn,addr = sk.accept()
-	while True:
-		accept_data = str(conn.recv(1024),encoding="utf8")
-		print("".join(["接收内容：", accept_data,]))
-		if accept_data=='bye':
-			break
-		#send
-		send_data = input()
-		conn.sendall(bytes(send_data,encoding="utf8"))
-	conn.close()
+import socketserver
+class myserver(socketserver.BaseRequestHandler):
+	def handle(self):
+		while True:
+			conn = self.request
+			addr = self.client_address
+			while True:
+				acc_data=str(conn.recv(1024),encoding="utf8")
+				recv_data = str(addr[1])+ ':'+ acc_data
+				conn.sendall(recv_data)
+			conn.close()
+if __name__ == '__main__':
+	sever = socketserver.ThreadingTCPServer(("127.0.0.1", 8888),myserver)
+	sever.serve_forever()
